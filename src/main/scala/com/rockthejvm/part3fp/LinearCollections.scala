3dfa85ca05b34011c2b347fca9fc457c1d430adf
kkw855 <kkw855@gmail.com>
2022-11-14 11:01:16 +0900

    Studying

--- src/main/scala/com/rockthejvm/part3fp/LinearCollections.scala
+++ src/main/scala/com/rockthejvm/part3fp/LinearCollections.scala
@@ -1,5 +1,7 @@
 package com.rockthejvm.part3fp
 
+import scala.util.Random
+
 //noinspection ScalaUnusedSymbol,SimplifiableFoldOrReduce
 object LinearCollections {
 
@@ -24,6 +26,7 @@ object LinearCollections {
     println(sortedSequence)
   }
 
+  // lists
   def testLists(): Unit = {
     val aList = List(1,2,3)
     // same API as Seq
@@ -36,8 +39,74 @@ object LinearCollections {
     val scalax5 = List.fill(5)("Scala")
   }
 
+  // ranges
+  def testRanges(): Unit = {
+    val aRange: Range.Inclusive = 1 to 10
+    val aNonInclusiveRange: Range = 1 until 10 // 10 not included
+    // same Seq API
+    (1 to 10).foreach(_ => println("Scala"))
+  }
+
+  // arrays
+  def testArrays(): Unit = {
+    val anArray = Array(1,2,3,4,5,6) // int[] on the JVM
+    // most Seq APIs
+    // arrays are not Seqs
+    val aSequence: Seq[Int] = anArray.toIndexedSeq
+    // arrays are mutable
+    anArray.update(2, 30) // no new array is allocated
+  }
+
+  // vectors = fast Seqs for a large amount of data
+  def testVectors(): Unit = {
+    val aVector: Vector[Int] = Vector(1,2,3,4,5,6)
+    // the same Seq API
+  }
+
+  def smallBenchmark(): Unit = {
+    val maxRuns = 1000
+    val maxCapacity = 1000000
+
+    def getWriteTime(collection: Seq[Int]): Double = {
+      val random = new Random()
+      val times = for {
+        i <- 1 to maxRuns
+      } yield {
+        val index = random.nextInt(maxCapacity) // non-inclusive
+        val element = random.nextInt()
+
+        val currentTime = System.nanoTime()
+        val updatedCollection = collection.updated(index, element)
+        System.nanoTime() - currentTime
+      }
+
+      // compute average
+      times.sum * 1.0 / maxRuns
+    }
+
+    val numbersList = (1 to maxCapacity).toList
+    val numbersVector = (1 to maxCapacity).toVector
+
+    println(getWriteTime(numbersList))
+    println(getWriteTime(numbersVector))
+  }
+
+  // sets
+  def testSets(): Unit = {
+    val aSet = Set(1,2,3,4,5,4) // no ordering guaranteed
+    // equals + hashCode = hash set
+    // main API: test if in the set
+    val contains3 = aSet.contains(3) // true
+    val contains3_v2 = aSet(3) // same: true
+    // adding/removing
+    val aBiggerSet = aSet + 4 // [1,2,3,4,5]
+    val aSmallerSet = aSet - 4 // [1,2,3,5]
+    // concatenation
+    val anotherSet = Set(4,5,6,7,8)
+    val muchBiggerSet = aSet ++ anotherSet
+  }
+
   def main(args: Array[String]): Unit = {
-    testSeq()
-    testLists()
+    smallBenchmark()
   }
 }
